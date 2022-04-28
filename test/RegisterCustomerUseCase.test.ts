b27665973241b91eb185418ce000a73fc75d793e
Olaf Thielke <olaf.thielke@gmail.com>
2022-04-29 11:47:51 +1200

    Can construct RegisterCustomerUseCase - Red (Compile)

---
+++ test/RegisterCustomerUseCase.test.ts
@@ -0,0 +1,6 @@
+describe("When construct RegisterCustomerUseCase", () => {
+    
+    it("Then can construct RegisterCustomerUseCase.", () => {
+        const useCase = new RegisterCustomerUseCase();
+    });
+});

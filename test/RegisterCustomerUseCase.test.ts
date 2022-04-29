63d05f104af9abc1149432e83379447be51cdb01
Olaf Thielke <olaf.thielke@gmail.com>
2022-04-29 12:13:37 +1200

    Can call register() - Red (Compile)

--- test/RegisterCustomerUseCase.test.ts
+++ test/RegisterCustomerUseCase.test.ts
@@ -3,6 +3,14 @@ import RegisterCustomerUseCase from "../src/RegisterCustomerUseCase";
 describe("When construct RegisterCustomerUseCase", () => {
     
     it("Then can construct RegisterCustomerUseCase.", () => {
-        const useCase = new RegisterCustomerUseCase();
+        const usecase = new RegisterCustomerUseCase();
+    });
+});
+
+describe("When call register()", () => {
+
+    it("Then can call register().", () => {
+        const usecase = new RegisterCustomerUseCase();
+        usecase.register();
     });
 });

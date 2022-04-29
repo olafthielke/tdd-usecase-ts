9f0ae8540a3b115e8e16a620d021b7226d13cb99
Olaf Thielke <olaf.thielke@gmail.com>
2022-04-29 12:38:48 +1200

    Remove RegisterCustomerUseCase construction scaffolding test - Refactor

--- test/RegisterCustomerUseCase.test.ts
+++ test/RegisterCustomerUseCase.test.ts
@@ -2,13 +2,6 @@ import RegisterCustomerUseCase from "../src/RegisterCustomerUseCase";
 import { MissingFirstName } from "../src/errors";
 
 
-describe("When construct RegisterCustomerUseCase", () => {
-    
-    it("Then can construct RegisterCustomerUseCase.", () => {
-        const usecase = new RegisterCustomerUseCase();
-    });
-});
-
 describe("When call register()", () => {
 
     it("without customer.firstname, Then throw MissingFirstName error.", () => {
